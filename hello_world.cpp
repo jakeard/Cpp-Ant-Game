a9ceadc938c6b9b1f45eaa0c4d7f1d29767cc5d3
Jake Ard <ard20001@byui.edu>
2021-10-05 22:26:46 -0600

    Add project files.

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string pizza;
+
+	cout << "What is your favorite pizza? \n";
+	cin >> pizza;
+	cout << "\nYour favorite pizza is " + pizza;
+	return 0;
+}
